43f5cd204a49aefcdcd4f570ab52c262f0debf2b
B_jiangyuan <B_jiangyuan>
2023-06-12 11:04:34 +0800

    add:pro

---
+++ Assets/UPMSample/Runtime/UPMSample.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UPMSample
+{
+    public static void CallUPM() {
+        Debug.Log("Call UPM");
+    }
+	
+	public static void CallUPM_Version() {
+        Debug.Log("Call UPM Version");
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-README
+# sample_upm
+
+#### 介绍
+测试用UPM
